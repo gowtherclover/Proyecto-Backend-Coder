5e71bff1057af5a7826a994f158cf794ecaef08c
Enzo Aguilar <96enzoaguilar@gmail.com>
2023-05-04 21:22:06 -0300

    use productoManager.js and GET, POST, PUT, DELETE

--- server.js
+++ server.js
@@ -1,44 +1,96 @@
-/* const http = require("http")
-
-const server = http.createServer((req,res)=>{
-    console.log("hora " + Date.now());
-    res.end("hola mundo")
-})
-
-server.listen(8080,()=>{
-    console.log("Server is running on http://localhost:8080");
-}) */
 const express = require("express")
-//importamos el productmanager que esta en otro archivo
+const ProductManager = require('./productManager')
 
 const app = express()
 const PORT = 4000
 
+app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
-app.get('/products',(req,res)=>{
-    res.json()
-})
+const productManager = new ProductManager('data.json')
 
-let animales=[
-    {id:1,animal:'leon',caracteristica:'4 patas'},
-    {id:2,animal:'lobo',caracteristica:'4 patas'},
-    {id:3,animal:'perro',caracteristica:'4 patas'},
+let products=[
+    {id:'660862793794',name:'remera',price:750},
+    {id:'660342793794',name:'pantalon',price:750},
+    {id:'660562793794',name:'jean',price:750},
+    {id:'660762793794',name:'zapatilla',price:750},
+    {id:'660762793794',name:'buzo',price:750},
+    {id:'660122793794',name:'buzo rojo',price:750},
+    {id:'660342793794',name:'remera roja',price:750},
+    {id:'660552793794',name:'jean negro',price:750},
 ]
 
-app.get('/animales/:id',(req,res)=>{
+//INICIO ENDPOINT PRODUCTS
+app.get('/products/:id',(req,res)=>{
     const id=req.params.id
-    const animalEncontrado = animales.find((c)=>c.id == id)
-    res.json(animalEncontrado)
+    //const productoEncontrado = products.find((c)=>c.id == id)
+    const productoEncontrado = productManager.getProductById(id)
+    return res
+    .status(200)
+    .json({status:"success", msg:'Producto encontrado',data:productoEncontrado})
 })
 
-app.get('/animales',(req,res)=>{
-    const query = req.query
-    console.log(query);
-    res.send('animal X')
+app.get('/products',(req,res)=>{
+    const allProducts = productManager.getProducts()
+    return res
+    .status(200).
+    json({status:"success", msg:'todos los productos',data:allProducts})
+})
+
+//BORRAR UN PRODUCTO (NECEISTO PASAR ID)
+app.delete('/products/:id',(req,res)=>{
+    const id=req.params.id
+    //products = products.filter((p)=>p.id != id)
+    const deletedProduct = productManager.deleteProduct(id)
+    return res
+    .status(200).
+    json({status:"success", msg:'producto eliminado',data:deletedProduct})
+})
+
+//CREAR UN PRODUCTO (NO NECEISTO PASAR ID)
+app.post('/products',(req,res)=>{
+    const producto = req.body
+    //producto.id = (Math.random () * 1000000000000).toFixed(0)
+    //products.push(producto)
+    const createdProduct = productManager.addProduct(producto)
+    return res
+    .status(201).
+    json({status:"success", msg:'producto creado',data:createdProduct})
+})
+
+//MODIFICAR UN PRODUCTO (NECEISTO PASAR ID)
+app.put('/products/:id',(req,res)=>{
+    const id=req.params.id
+    const newBody=req.body
+
+    /* const productIndex = products.findIndex((prod) => prod.id === id)
+        if (productIndex === -1) {
+            console.log('Producto para actualizar no encontrado')
+            return false
+        }
+        const product = { id:products[productIndex].id, ...newBody }
+        products[productIndex] = product */
+    const updatedProduct = productManager.updateProduct(id, newBody)
+    if (!updatedProduct) {
+        console.log('Producto para actualizar no encontrado')
+        return res
+        .status(404)
+        .json({status:"error", msg:'Producto para actualizar no encontrado',data:{}})
+    }
+
+    return res
+    .status(200).
+    json({status:"success", msg:'producto modificado',data:updatedProduct})
+})
+//FIN ENDPOINT PRODUCTS
+
+app.get('*', (req,res)=>{
+    return res
+    .status(404)
+    .json({status:"error", msg:'no se encuentra esa ruta',data:{}})
 })
 
 
 app.listen(PORT,()=>{
-    console.log(`escuchando en el servidor puerto ${PORT}`);
+    console.log(`escuchando en el servidor puerto http://localhost:${PORT}`);
 })

c101468dd0bdb36c3c85569e1483dccc955511e5
Enzo Aguilar <96enzoaguilar@gmail.com>
2023-06-01 21:07:10 -0300

    agregando mongoose y validaciones

---
+++ src/routes/users.routes.js
@@ -0,0 +1,102 @@
+import express from "express";
+import { UserModel } from "../models/users.model.js";
+export const usersRouter = express.Router();
+
+usersRouter.get("/", async (req, res) => {
+  try {
+    const users = await UserModel.find({});
+    return res.status(200).json({
+      status: "success",
+      msg: "listado de usuarios",
+      data: users,
+    });
+  } catch (e) {
+    console.log(e);
+    return res.status(500).json({
+      status: "error",
+      msg: "something went wrong :(",
+      data: {},
+    });
+  }
+});
+
+usersRouter.post("/", async (req, res) => {
+  try {
+    const { firstName, lastName, email } = req.body;
+    if (!firstName || !lastName || !email) {
+      console.log(
+        "validation error: please complete firstName, lastname and email."
+      );
+      return res.status(400).json({
+        status: "error",
+        msg: "please complete firstName, lastname and email.",
+        data: {},
+      });
+    }
+    const userCreated = await UserModel.create({ firstName, lastName, email });
+    return res.status(201).json({
+      status: "success",
+      msg: "user created",
+      data: userCreated,
+    });
+  } catch (e) {
+    console.log(e);
+    return res.status(500).json({
+      status: "error",
+      msg: "something went wrong :(",
+      data: {},
+    });
+  }
+});
+
+usersRouter.put("/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { firstName, lastName, email } = req.body;
+    if (!firstName || !lastName || !email || !id) {
+      console.log(
+        "validation error: please complete firstName, lastname and email."
+      );
+      return res.status(400).json({
+        status: "error",
+        msg: "please complete firstName, lastname and email.",
+        data: {},
+      });
+    }
+    const userUptaded = await UserModel.updateOne(
+      { _id: id },
+      { firstName, lastName, email }
+    );
+    return res.status(201).json({
+      status: "success",
+      msg: "user uptaded",
+      data: userUptaded,
+    });
+  } catch (e) {
+    console.log(e);
+    return res.status(500).json({
+      status: "error",
+      msg: "something went wrong :(",
+      data: {},
+    });
+  }
+});
+
+usersRouter.delete("/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const deleted = await UserModel.deleteOne({ _id: id });
+    return res.status(200).json({
+      status: "success",
+      msg: "user deleted",
+      data: {},
+    });
+  } catch (e) {
+    console.log(e);
+    return res.status(500).json({
+      status: "error",
+      msg: "something went wrong :(",
+      data: {},
+    });
+  }
+});
--- src/utils.js
+++ src/utils.js
@@ -1,6 +1,7 @@
 import multer from "multer";
 import path from "path"
 import {fileURLToPath} from "url"
+import { connect } from "mongoose";
 export const __filename = fileURLToPath(import.meta.url)
 export const __dirname = path.dirname(__filename)
 
@@ -14,6 +15,19 @@ const storage = multer.diskStorage({
         cb(null, file.originalname);
     },
 });
+
+
+export async function connectMongo() {
+    try {
+      await connect(
+        "mongodb+srv://96enzoaguilar:FWSadN7jvyazwmaC@backendcoder.uzv2r7b.mongodb.net/?retryWrites=true&w=majority"
+      );
+      console.log("plug to mongo!");
+    } catch (e) {
+      console.log(e);
+      throw "can not connect to the db";
+    }
+  }
 
 export const uploader = multer({ storage });
 
